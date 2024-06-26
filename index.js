56afb8c2933a7cf0420c9fe58a1e04a16550a21d
aanasakhtar <anasakhtar851@gmail.com>
2024-06-26 18:53:54 +0500

    final

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getComputerChoice(choices) {
     let rand_id = Math.floor(Math.random() * choices.length);
-    return choices[rand_id]
+    return choices[rand_id];
 }
 
 const buttons = document.querySelectorAll("button");
@@ -9,72 +9,65 @@ const comp = document.querySelector(".computer-choice");
 const userTextRef = document.querySelector(".user-text");
 const compTextRef = document.querySelector(".comp-text");
 
-function getUserChoice(event) {
-    const btnText = event.target.textContent;
-    return btnText;
-}
-
 const choices = ["Rock", "Paper", "Scissors"];
 let compScore = 0;
 let userScore = 0;
 let round = 0;
-
-
+const totalRounds = 5;
 
 function pointTheGame(compChoice, userChoice) {
-    if (compChoice == userChoice) {
+    if (compChoice === userChoice) {
         console.log("No point for anyone");
     }
-    else if (compChoice == "Rock" && userChoice == "Paper") {
+    else if (
+        (compChoice === "Rock" && userChoice === "Paper") ||
+        (compChoice === "Paper" && userChoice === "Scissors") ||
+        (compChoice === "Scissors" && userChoice === "Rock")
+    ) {
         console.log("Point for User!");
         userScore += 1;
     }
-    else if (compChoice == "Rock" && userChoice == "Scissors") {
+    else {
         console.log("Point for Computer!");
         compScore += 1;
     }
-    else if (compChoice == "Paper" && userChoice == "Scissors") {
-        console.log("Point for User!");
-        userScore += 1;
-    }
-    else if (compChoice == "Paper" && userChoice == "Rock") {
-        console.log("Point for Computer!");
-        compScore += 1;
-    }
-    else if (compChoice == "Scissors" && userChoice == "Rock") {
-        console.log("Point for User!");
-        userScore += 1;
-    }
-    else if (compChoice == "Scissors" && userChoice == "Paper") {
-        console.log("Point for Computer!");
-        compScore += 1;
-    }
-    
+
     user.textContent = `Your choice: ${userChoice}`;
     comp.textContent = `Computer choice: ${compChoice}`;
-    
+
     userTextRef.textContent = `Player's score: ${userScore}`;
     compTextRef.textContent = `Computer's Score: ${compScore}`;
 }
 
-function playTheGame(gamerounds = 5) {
-    while (round < gamerounds || userScore == compScore) {
-        let userChoice;       
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {userChoice = getUserChoice(event)});
-        });
-        let compChoice = getComputerChoice(choices);
-        
-        pointTheGame(compChoice, userChoice);
+function playTheGame() {
+    buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const userChoice = event.target.textContent;
+            const compChoice = getComputerChoice(choices);
 
-        round++;
-    }
-    if (userScore > compScore) {
-        alert("You won!")
-    }
-    else {
-        alert("Sorry you lost")
-    }    
+            pointTheGame(compChoice, userChoice);
+            round++;
+
+            if (round >= totalRounds) {
+                if (userScore == compScore) {
+                    alert("That's a draw, play one more!");
+                    round = totalRounds - 1;
+                }
+                else {
+                    if (userScore > compScore) {
+                        alert("You won!");
+                    }
+                    else {
+                        alert("Sorry you lost");
+                    }
+    
+                    userScore = 0;
+                    compScore = 0;
+                    round = 0;
+                }
+            }
+        });
+    });
 }
 
 playTheGame();
